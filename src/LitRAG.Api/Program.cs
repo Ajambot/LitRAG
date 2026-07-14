5031d39de3978571a19d165d111327ca56a3b319
Martin Morales <mmorale5@lakeheadu.ca>
2026-07-14 18:33:30 -0400

    Add chunking logic with overlap

--- src/LitRAG.Api/Program.cs
+++ src/LitRAG.Api/Program.cs
@@ -1,4 +1,5 @@
 using LitRAG.Core;
+using UglyToad.PdfPig.Content;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -11,8 +12,24 @@ app.MapGet("/pdf", () =>
     if (!File.Exists(path))
         return Results.NotFound($"File not found: {path}");
 
-    List<string> pageInfo = Class1.ReadPdf(path);
-    return Results.Ok(pageInfo);
+    List<IEnumerable<Word>> pages = PDFMgr.ReadPdf(path);
+    List<string> resp = [];
+    foreach (IEnumerable<Word> page in pages)
+    {
+        resp.Add(string.Join(" ", page.Take(500)));
+    }
+    return Results.Ok(resp);
+});
+
+app.MapGet("/chunk", () =>
+{
+    string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+    string path = Path.Combine(home, "Personal/LitRAG/src/LitRAG.Core/Sample Papers/Kalantari 2023 Understanding-the-Language-of-ADHD-and-Autism-Communities-on-Social-Media.pdf");
+
+    if (!File.Exists(path))
+        return Results.NotFound($"File not found: {path}");
+
+    return Results.Ok(PDFMgr.ChunkPdf(path, 350, 50));
 });
 
 app.Run();
---
+++ src/LitRAG.Core/PDFMgr.cs
@@ -0,0 +1,47 @@
+﻿namespace LitRAG.Core;
+
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.WordExtractor;
+
+public class PDFMgr
+{
+	public static List<IEnumerable<Word>> ReadPdf(string path)
+	{
+		using PdfDocument document = PdfDocument.Open(path);
+		List<IEnumerable<Word>> pageInfo = [];
+		foreach (Page page in document.GetPages())
+		{
+			IEnumerable<Word> words = page.GetWords(NearestNeighbourWordExtractor.Instance);
+			pageInfo.Add(words);
+		}
+		return pageInfo;
+	}
+
+	// chunkSize - # of words each chunk should be
+	// overlap - # of words that overlap between adjacent chunks
+	public static List<string> ChunkPdf(string path, int chunkSize, int overlap)
+	{
+		using PdfDocument document = PdfDocument.Open(path);
+		List<string> chunks = [];
+		var buf = new List<Word>(chunkSize);
+		foreach (Page page in document.GetPages())
+		{
+			foreach (Word word in page.GetWords())
+			{
+				buf.Add(word);
+				if (buf.Count == chunkSize)
+				{
+					chunks.Add(string.Join(" ", buf.ToList()));
+					buf.RemoveRange(0, chunkSize - overlap);
+				}
+			}
+		}
+
+		if (buf.Count != 0)
+		{
+			chunks.Add(string.Join(" ", buf.ToList()));
+		}
+		return chunks;
+	}
+}
